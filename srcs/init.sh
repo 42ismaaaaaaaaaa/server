3deea9bbd65fccdd6d060d8c63a52b63fbca2af3
Aamsi <ismaelouali45@gmail.com>
2021-03-08 18:02:59 +0100

    implements autoindex

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,10 +1,18 @@
-service nginx start
 service mysql start
-service php7.3-fpm start
 
 echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 echo "UPDATE mysql.user set plugin='' WHERE user='root';" | mysql -u root --skip-password
+
+if [[ $AUTOINDEX == "off" ]]
+then
+    sed -i -r 's/.autoindex./autoindex off;/g' /etc/nginx/sites-available/default
+else
+    sed -i -r 's/.autoindex./autoindex on;/g' /etc/nginx/sites-available/default
+fi
+
+service nginx start
+service php7.3-fpm start
 
 bash
